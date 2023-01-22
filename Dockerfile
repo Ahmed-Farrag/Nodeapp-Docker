028b09b72cf49495106c098081600f5054f66c8a
Ahmed Farrag <47854268+Ahmed-Farrag@users.noreply.github.com>
2023-01-22 20:11:15 +0200

    yml files

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ COPY package.json .
 
 RUN npm install 
 
-COPY src/server.js .
+COPY  . .
+
+EXPOSE 4000
 
 CMD ["npm", "start"]
--- src/server.js
+++ src/server.js
@@ -4,5 +4,5 @@ const app = express();
 
 app.get("/", (req, res) => res.send("<h1>hello addy how are</h1>"));
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`app is up and running on : ${PORT}`));
